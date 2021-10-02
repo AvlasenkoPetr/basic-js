8733496d8ee7b743ba36e7923f8ce6fc5581042f
AvlasenkoPetr <phone.petr@gmail.com>
2021-10-02 14:53:36 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (+sampleActivity != 'number') return false
+  let n = Math.log( MODERN_ACTIVITY / sampleActivity );
+  let k = (0.693 / HALF_LIFE_PERIOD);
+  return Math.ceil(n / k)
 }

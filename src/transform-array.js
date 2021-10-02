fcb602ff79293d0b59c0bcd2a386b188f5457507
AvlasenkoPetr <phone.petr@gmail.com>
2021-10-02 19:19:11 +0300

    feat: add tranform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,64 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+  
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch(arr[i]) {
+      case '--discard-next':
+        i += 1;
+        continue;
+
+      case '--discard-prev':
+        resultArr.pop();
+        continue;
+
+      case '--double-next':
+        resultArr.push(arr[i+1])
+        continue;
+
+      case '--double-prev':
+        if (i = 0) {
+         continue;
+        }
+        resultArr.push(resultArr[resultArr.length - 1])
+        
+      default: resultArr.push(arr[i])
+    }
+
+
+
+    // if (arr[i] == '--discard-next') {
+    //   i += 1;
+
+    // } else if (arr[i] == '--discard-prev') {
+    //   resultArr.pop()
+
+    // } else if (arr[i] == '--double-next') {
+    //   resultArr.push(arr[i+1])
+
+    // } else if (arr[i] == '--double-prev') {
+    //   if (i = 0) {
+    //     continue;
+    //   } else {
+    //   resultArr.push(resultArr[resultArr.length - 1])
+    //   }
+    // } else {
+    //   resultArr.push(arr[i])
+    // }
+  }
+  return resultArr;
 }
+
+let test = [1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]
+
+// // console.log(test[3])
+
+// let newArr = []
+
+// newArr.pop()
+
+console.log(transform(test))

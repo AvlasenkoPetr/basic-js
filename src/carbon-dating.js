2ee15db995b2fdaa5414dde7ada32971ef74b9b5
AvlasenkoPetr <phone.petr@gmail.com>
2021-10-02 17:18:45 +0300

    fix: correct carbon-dating error

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,21 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(sampleActivity) {
-  if (isNaN(sampleActivity) || +sampleActivity != 'number' || +sampleActivity > MODERN_ACTIVITY || +sampleActivity < 0) return false
-  let n = Math.log( MODERN_ACTIVITY / Math.round(sampleActivity) );
+  if ( !sampleActivity 
+      || typeof(sampleActivity) != 'string'  
+      || isNaN(+sampleActivity)
+      || +sampleActivity === undefined
+      || +sampleActivity > 15 
+      || +sampleActivity <= 0)
+      return false;
+
+  let n = Math.log( MODERN_ACTIVITY / sampleActivity );
   let k = (0.693 / HALF_LIFE_PERIOD);
   return Math.ceil(n / k)
 }
+
+let test = '12000'
+
+console.log(isNaN(+test))
+
+

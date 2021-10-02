f6914119ae5cf175aafbf41422db4321681e56ad
AvlasenkoPetr <phone.petr@gmail.com>
2021-10-02 15:02:36 +0300

    fix: correct carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,8 +18,8 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(sampleActivity) {
-  if (+sampleActivity != 'number') return false
-  let n = Math.log( MODERN_ACTIVITY / sampleActivity );
+  if (isNan(sampleActivity)) return false
+  let n = Math.log( MODERN_ACTIVITY / +sampleActivity );
   let k = (0.693 / HALF_LIFE_PERIOD);
   return Math.ceil(n / k)
 }

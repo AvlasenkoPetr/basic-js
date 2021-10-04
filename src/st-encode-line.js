407c3181d42a9d9aeb5402813995a1884972d834
AvlasenkoPetr <phone.petr@gmail.com>
2021-10-04 23:19:47 +0300

    feat: add encode-line solution

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  let result = []
+  let count = 1
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) {
+      count += 1
+    } else {
+      if (count > 1) {
+        let code = count + str[i];
+        result.push(code);
+        count = 1;
+      } else {
+        result.push(str[i])
+        count = 1;
+      }
+    }
+  }
+  return result.join('')
 }
+
+console.log(encodeLine('abbcca'))
